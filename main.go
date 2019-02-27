6394b0c2f39678d7c5f61688a0e9fec6d4249873
mikle9997 <mikle9997@mail.ru>
2019-02-27 20:33:54 +0300

    simple page

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GET /")
+	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "index", nil)
+}
 
 func main() {
 	fmt.Println("Listening on port : 3000")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(*r)
-		fmt.Fprintf(w, "<h1>Hello world!</h1>")
-	})
+	http.HandleFunc("/", indexHandler)
 
 	http.ListenAndServe(":3000", nil)
 }
